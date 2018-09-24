07c357b9a05b513fca66b7c7060254f388604374
Tomas <tomasgarzonhervas@gmail.com>
2018-09-24 18:10:43 +0200

    Adding group and credential operation

---
+++ pyaccredible/pyaccredible/client.py
@@ -0,0 +1,80 @@
+import requests
+
+
+class AccredibleWrapper:
+
+    PRODUCTION_API_URL = 'https://api.accredible.com/v1/'
+
+    def __init__(self, key, server=None):
+        self.key = key
+        if not server:
+            self.API_URL = self.PRODUCTION_API_URL
+        else:
+            self.API_URL = server
+
+    def build_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token token={}'.format(self.key)
+        }
+
+    @property
+    def group_url(self):
+        return '{}issuer/groups'.format(self.API_URL)
+
+    def get(self, url, data):
+        return requests.get(url, data=data, headers=self.build_headers())
+
+    def post(self, url, data):
+        return requests.post(url, data=data, headers=self.build_headers())
+
+    def put(self, url, data):
+        return requests.put(url, data=data, headers=self.build_headers())
+
+    def delete(self, url, data):
+        return requests.delete(url, headers=self.build_headers())
+
+    def group_create(self, **kwargs):
+        data = {
+            'group': kwargs
+        }
+        url = self.group_url
+        return self.post(url, data)
+
+    def credential_create(self, group_id, name, email, issued_on):
+        data = {
+            'credential': {
+                'recipient': {
+                    'name': name,
+                    'email': email,
+                },
+                'group_id': group_id,
+                'issued_on': issued_on.strftime("%Y-%m-%d")
+            }
+        }
+        url = '{}credentials'.format(self.API_URL)
+        return self.post(url, data)
+
+    def credential_update(self, credential_id, **kwargs):
+        data = {
+            'credential': kwargs
+        }
+        url = '{}credentials/{}'.format(self.API_URL, credential_id)
+        return self.put(url, data)
+
+    def credential_create_bulk(self, group_id, participants, issued_on):
+        data = {
+            'credentials': []
+        }
+        for participant in participants:
+            recipient = {
+                'recipient': {
+                    'name': participant[0],
+                    'email': participant[1],
+                },
+                'group_id': group_id,
+                'issued_on': issued_on.strftime("%Y-%m-%d")
+            }
+            data['credentials'].append(recipient)
+        url = '{}credentials/bulk_create'.format(self.API_URL)
+        return self.post(url, data)
--- pyaccredible/tests/test_pyaccredible.py
+++ pyaccredible/tests/test_pyaccredible.py
@@ -5,8 +5,9 @@
 
 
 import unittest
+from datetime import datetime
 
-from pyaccredible import pyaccredible
+from pyaccredible.client import AccredibleWrapper
 
 
 class TestPyaccredible(unittest.TestCase):
@@ -14,9 +15,52 @@ class TestPyaccredible(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        self.client = AccredibleWrapper('')
+        self.client.API_URL = 'https://private-anon-3d8bf32214-accrediblecredentialapi.apiary-mock.com/v1/'
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+    def test_create_group(self):
         """Test something."""
+        data = {
+            "name": "new group",
+            "course_name": "Intro to Prgramming",
+            "course_description": "Description of course",
+            "course_link": "http://www.example.com",
+            "language": "en",
+            "attach_pdf": True,
+            "design_id": None
+        }
+
+        response = self.client.group_create(**data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_credencial(self):
+        """Test something."""
+        data = {
+            'name': 'First Name',
+            'email': 'name@example.org',
+            'group_id': 999,
+            'issued_on': datetime(2018, 10, 10)
+        }
+
+        response = self.client.credential_create(**data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_credential(self):
+        data = {'approved': True}
+        response = self.client.credential_update(credential_id=999, **data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_bulk_create_credencial(self):
+        """Test something."""
+        data = {
+            'participants': [],
+            'group_id': 999,
+            'issued_on': datetime(2018, 10, 10)
+        }
+        data['participants'].append(['name1', 'name1@example.org'])
+        data['participants'].append(['name2', 'name2@example.org'])
+        response = self.client.credential_create_bulk(**data)
+        self.assertEqual(response.status_code, 200)
