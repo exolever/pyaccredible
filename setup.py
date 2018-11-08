3978a05c0f67d8fc7453272faf6ba86075c6bed4
Tomas <tomasgarzonhervas@gmail.com>
2018-11-08 16:04:32 +0100

    Bump version: 0.4.1 → 0.5.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/exolever/pyaccredible',
-    version='0.4.1',
+    version='0.5.0',
     zip_safe=False,
 )
